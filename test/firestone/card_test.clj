c3c0d2186ca94ba2718ee82461868caf6aafda47
Sebastian Gedin <sgedin@kth.se>
2018-11-14 20:02:25 +0100

    Add card test file and test for AoP Fix #45

---
+++ test/firestone/card_test.clj
@@ -0,0 +1,18 @@
+(ns firestone.card_test
+  (:require [ysera.test :refer [deftest is is-not is= error?]]
+            [firestone.api :refer [attack-with-minion]]
+            [firestone.definitions :refer [get-definition]]
+            [firestone.core :refer []]
+            [firestone.construct :refer [create-game
+                                         create-minion
+                                         get-hand
+                                         create-hero]]))
+
+(deftest acolyte-of-pain
+         "Tests for Acolyte of Pain."
+         (is= (-> (create-game [{:minions [(create-minion "Acolyte of Pain" :id "ap")] :deck ["Imp"]}
+                                {:minions [(create-minion "Imp" :id "imp")]}])
+                  (attack-with-minion "ap" "imp")
+                  (get-hand "p1")
+                  (count))
+              1))
